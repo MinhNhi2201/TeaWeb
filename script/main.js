f88e3056841a001bc224ba4b5f7a2cc45aca73ce
Nguyen Phan Anh Duc <npanhduc@gmail.com>
2022-12-27 10:50:54 +0700

    update main

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-$(".drop")
+/*$(".drop")
   .mouseover(function() {
   $(".dropdown").show(300);
 });
@@ -6,3 +6,58 @@ $(".drop")
   .mouseleave(function() {
   $(".dropdown").hide(300);     
 });
+*/
+
+//slideshow NEWS
+$(document).ready(function(){
+  $('.news').slick({
+    dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+  });
+});
+
+
+//slideshow DANH MUC SP
+
+$('.list').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2
+});
+
+
+	
+
+
+
+     
+
